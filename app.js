5e92da4f7ae6d7d3a83cc92e4f26d4f124e36df1
Niyati Thakkar <example@gmail.com>
2023-01-18 23:15:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+const https = require("https");
+// list id - 005c18994f
+//api key - 0956d4f96bb3b4b92702921084a3a9ca-us21
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function(req,res){
+  const fn = req.body.fname;
+  const ln = req.body.lname;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fn,
+          LNAME: ln
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us21.api.mailchimp.com/3.0/lists/005c18994f/";
+  const options = {
+    method : "POST",
+    auth: "niyatit:0956d4f96bb3b4b92702921084a3a9ca-us21"
+  }
+  const request = https.request(url, options, function(response){
+    if(response.statusCode == 200){
+      res.sendFile(__dirname + "/success.html");
+    }
+    else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.sendFile(__dirname+"signup.html");
+});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server is running on port 3000");
+});
